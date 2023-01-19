e1fba08f888def978b9f7914c9123269ec5e3d8e
Davi R <115122924+DvRibeiro@users.noreply.github.com>
2023-01-19 19:03:02 -0300

    Update and rename 01.rb to number greater than.rb

--- number greater than.rb
+++ number greater than.rb
@@ -2,13 +2,11 @@
 #In Ruby speak, you could put it like this: Given an array, count how many items are greater than 5.
 
 array = [17, 7, 3, 6, 10, 1]
-
 count = 0 
+
 for number in array
     if number > 5
         count += 1
     end
 end
-puts " "
 puts "There are #{count} items greater than 5."
-puts " "
