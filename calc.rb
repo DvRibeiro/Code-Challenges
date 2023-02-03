c60610611668a631f0820f954213f6f65b48f403
Davi R <115122924+DvRibeiro@users.noreply.github.com>
2023-02-03 01:06:13 -0300

    Update calc.rb

--- calc.rb
+++ calc.rb
@@ -1,7 +1,7 @@
-resultado = " "
+result = " "
 
 loop do
-    puts resultado
+    puts result
     puts "   ______________________________________________"
     puts "  |Welcome to my calculator!                     |"
     puts "  |Select one option:                            |"
@@ -13,25 +13,25 @@ loop do
     option = gets.chomp.to_i
 
     if option == 1
-        puts "Type the operation you wanna do: "
-        operation = gets.chomp.split    
+        print "Type the operation you wanna do: "
+        operation1 = gets.chomp.split    
         
-    num1 = operation[0].to_f
-    op = operation[1]
-    num2 = operation[2].to_f
+    num1 = operation1[0].to_f
+    op = operation1[1]
+    num2 = operation1[2].to_f
     
 
         case op
             when "+"
-            result = (num1 + num2) 
+            result_operation1 = (num1 + num2) 
             when "-"
-            result = (num1 - num2)
+            result_operation1 = (num1 - num2)
             when "*"
-            result = (num1 * num2)
+            result_operation1 = (num1 * num2)
             when "/"
-            result = (num1 / num2)
+            result_operation1 = (num1 / num2)
             end 
-            resultado = "The result is #{result}"
+            result = "The result is #{result_operation1}"
     
     elsif option == 2
         puts "Type your equation: "
@@ -49,9 +49,9 @@ loop do
         else 
             x1 = (-b + Math.sqrt(delta)) / (2*a)
             x2 = (-b - Math.sqrt(delta)) / (2*a)
-            result = x1 , x2 
+            result_operation2 = x1 , x2 
         end
-        resultado = "The result is S = #{result}"
+        result = "The result is S = #{result_operation2}"
     else
         break
     end
