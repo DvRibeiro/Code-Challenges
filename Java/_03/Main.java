24d0ba540852fd487cbe2b2412186eafd83c4f77
Davi R <davirscunha@gmail.com>
2023-02-17 12:17:27 -0300

    Update Main.java

--- Java/_03/Main.java
+++ Java/_03/Main.java
@@ -1,10 +1,18 @@
-//Faça um programa que receba o peso e a altura de uma pessoa. Determine e escreva a massa corpórea da pessoa (peso em kg dividido pela altura ao quadrado).
 package _03;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner teclado = new Scanner(System.in);
+	System.out.print("Digite seu peso (kg): ");
+	int peso = teclado.nextInt();
+	System.out.print("Digite sua altura (cm): ");
+	double altura = teclado.nextDouble();
+	double altura_formatada = altura/100 * altura/100;
+	double imc = peso/(altura_formatada);
+	System.out.println(imc);
+	
 
 	}
 
