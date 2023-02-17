307568adf4f21fc81f5ebb146b285b053029c92b
Davi R <115122924+DvRibeiro@users.noreply.github.com>
2023-02-17 11:06:30 -0300

    Update number greater than.rb

--- Ruby/number greater than.rb
+++ Ruby/number greater than.rb
@@ -1,1 +1,12 @@
+#This problem involves finding the number of integers with a value greater than 5. 
+#In Ruby speak, you could put it like this: Given an array, count how many items are greater than 5.
 
+array = [17, 7, 3, 6, 10, 1]
+count = 0 
+
+for number in array
+    if number > 5
+        count += 1
+    end
+end
+puts "There are #{count} items greater than 5."
