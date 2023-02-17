552c95363abcab596dbb221320d394a8708b3705
Davi R <115122924+DvRibeiro@users.noreply.github.com>
2023-02-17 11:07:46 -0300

    Update calc.rb

--- Ruby/calc.rb
+++ Ruby/calc.rb
@@ -1,24 +1,24 @@
-resultado = " "
+result = " "
 
 loop do
-    puts resultado
+    puts result
     puts "   ______________________________________________"
-    puts "  |Seja bem vindo a calculadora de otário!       |"
-    puts "  |Seleciona o que deseja fazer:                 |"
-    puts "  |1- Calcular o resultado de uma operação básica|"
-    puts "  |2- Resolver uma equação quadrática            |"
-    puts "  |0- Sair                                       |"
+    puts "  |Welcome to my calculator!                     |"
+    puts "  |Select one option:                            |"
+    puts "  |1- Calculate a basic operation                |"
+    puts "  |2- Solve a quadratic equation                 |"
+    puts "  |0- Exit                                       |"
     puts " "
-    print " Sua opção: "
+    print " Your option: "
     option = gets.chomp.to_i
 
     if option == 1
-        print "Digite a operação que deseja realizar: "
+        print "Type the operation you wanna do: "
         operation1 = gets.chomp.split    
         
-        num1 = operation1[0].to_f
-        op = operation1[1]
-        num2 = operation1[2].to_f
+    num1 = operation1[0].to_f
+    op = operation1[1]
+    num2 = operation1[2].to_f
     
 
         case op
@@ -31,10 +31,10 @@ loop do
             when "/"
             result_operation1 = (num1 / num2)
             end 
-            resultado = "O resultado é #{result_operation1}"
+            result = "The result is #{result_operation1}"
     
     elsif option == 2
-        puts "Digite a sua equação: "
+        puts "Type your equation: "
         print "a= "
         a = gets.chomp.to_f
         print "b= "
@@ -45,13 +45,13 @@ loop do
         delta = ((b * b) - 4 * a * c)
         
         if(delta < 0)
-            print "Não existem raízes reais"
+            print "Do not exist real roots."
         else 
             x1 = (-b + Math.sqrt(delta)) / (2*a)
             x2 = (-b - Math.sqrt(delta)) / (2*a)
-            result_quadratica = x1 , x2 
+            result_operation2 = x1 , x2 
         end
-        resultado = "O resultado é S = #{result_quadratica}"
+        result = "The result is S = #{result_operation2}"
     else
         break
     end
