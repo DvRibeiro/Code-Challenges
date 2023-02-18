338af61104476b1063b12037637b74d29e9cafb2
Davi R <115122924+DvRibeiro@users.noreply.github.com>
2023-02-18 18:35:51 -0300

    Update Main.java

--- Java/_04/Main.java
+++ Java/_04/Main.java
@@ -1,3 +1,16 @@
+/*
+PROBLEMA:
+
+Um hotel deseja fazer uma promoção especial de final de semana, concedendo um desconto
+de 25% na diária. Sendo informados, através do teclado, o número de apartamentos do hotel e
+o valor da diária por apartamento para o final de semana completo, elabore um programa
+para calcular e escrever:
+-valor promocional da diária
+-valor total a ser arrecadado caso a ocupação neste final de semana atinja 100%
+-valor total a ser arrecadado caso a ocupação neste final de semana atinja 70%
+-valor que o hotel deixará de arrecadar em virtude da promoção, caso a ocupação atinja 100%
+*/
+
 package _04;
 import java.util.Scanner;
 
