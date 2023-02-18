47f20ef60e69265d02a947861dbc40a8d2b37aa7
Davi R <115122924+DvRibeiro@users.noreply.github.com>
2023-02-18 18:32:55 -0300

    Update Main.java

--- Java/beecrowd/ex1037/Main.java
+++ Java/beecrowd/ex1037/Main.java
@@ -1,3 +1,6 @@
+//You must make a program that read a float-point number and print a message saying in which of following intervals the number belongs: [0,25] (25,50], (50,75], (75,100].
+//If the read number is less than zero or greather than 100, the program must print the message “Fora de intervalo” that means "Out of Interval".
+
 package ex1037;
 import java.util.Scanner;
 
