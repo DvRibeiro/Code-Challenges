a28627dd76627535ebe9521fca3bd10b31899eb5
Davi R <115122924+DvRibeiro@users.noreply.github.com>
2023-02-23 01:01:50 -0300

    Update Main.java

--- Java/beecrowd/ex1041/Main.java
+++ Java/beecrowd/ex1041/Main.java
@@ -1,3 +1,11 @@
+//Write an algorithm that reads two floating values (x and y), which should represent the coordinates of a point in a plane. 
+//Next, determine which quadrant the point belongs, or if you are at one of the Cartesian axes or the origin (x = y = 0).
+//If the point is at the origin, write the message "Origem".
+//If the point is at X axis write "Eixo X", else if the point is at Y axis write "Eixo Y".
+
+
+
+
 package ex1041;
 import java.util.Scanner;
 
